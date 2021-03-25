c1618bda305d5720b24d300b3177948a70026057
Maky <franklinjaradev@gmail.com>
2021-03-24 20:26:55 -0400

    Recycler View working for Restaurant List

---
+++ app/src/main/java/com/example/assignment_4/RestaurantAdapter.java
@@ -0,0 +1,83 @@
+package com.example.assignment_4;
+
+import android.content.pm.ResolveInfo;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.MyViewHolder> {
+
+    private List<Restaurant> restaurantList = new ArrayList<>();
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        // Declare items layout
+        public TextView nameTextView;
+        public TextView locationTextView;
+        public RatingBar ratingRatingBar;
+
+        public MyViewHolder(View view){
+            super(view);
+
+            // Initialize items layout
+            nameTextView = view.findViewById(R.id.nameTextView);
+            locationTextView = view.findViewById(R.id.locationTextView);
+            ratingRatingBar = view.findViewById(R.id.ratingRatingBar);
+        }
+    }
+
+    // Adapter Constructor
+    public RestaurantAdapter(List<Restaurant> restaurantList) {
+        this.restaurantList = restaurantList;
+    }
+
+    // Instances of ViewHolder
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        // Initiate the individual item layout
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.restaurants_recyclerview_items, parent, false);
+
+        // Crate new instance of View Holder with the Inflated Individual
+        return new MyViewHolder(itemView);
+    }
+
+    // Copies data from the underlying collection to the individual item
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position){
+
+        Restaurant restaurant = restaurantList.get(position);
+
+        // Get data to copy into the layout
+        String name = restaurant.getName();
+        String location = restaurant.getLocation();
+        int rating = restaurant.getRating();
+
+        // Set data inside the view holder
+        holder.nameTextView.setText(name);
+        holder.locationTextView.setText(location);
+        holder.ratingRatingBar.setRating(rating);
+    }
+
+    // Return the number of items in the underlying collection
+    @Override
+    public int getItemCount() {
+        return restaurantList.size();
+    }
+
+    // Change the underlying data collection(not an override)
+    public void setData(List<Restaurant> restaurantList){
+        // set to the new collection
+        this.restaurantList = restaurantList;
+
+        // Notify the recycler view that the underlying data set has changed
+        notifyDataSetChanged();
+    }
+}
--- app/src/main/java/com/example/assignment_4/RestaurantListActivity.java
+++ app/src/main/java/com/example/assignment_4/RestaurantListActivity.java
@@ -1,14 +1,41 @@
 package com.example.assignment_4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RestaurantListActivity extends AppCompatActivity {
+
+    // Create member variables
+    private List<Restaurant> restaurantList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private RestaurantAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_list);
+
+        // Set up RecyclerView
+        recyclerView = findViewById(R.id.recyclerViewRestaurants);
+        adapter = new RestaurantAdapter(restaurantList);
+
+        RecyclerView.LayoutManager layoutManager =
+                new LinearLayoutManager(getApplicationContext());
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+
+        // Add test data
+        Restaurant r;
+        r = new Restaurant("The Shed", 3, "Sayville");
+        restaurantList.add(r);
+
+        adapter.notifyDataSetChanged();
     }
 }
